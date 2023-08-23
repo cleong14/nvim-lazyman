43d362228100215af59043bb4ec4ac4dafce2d2b
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2023-08-23 12:40:02 -1000

    wip: nvim-lazyman config
    Signed-off-by: Chaz Leong <13462818+cleong14@users.noreply.github.com>

--- lua/configuration.lua
+++ lua/configuration.lua
@@ -27,14 +27,14 @@ conf.enable_transparent = true
 
 -- GLOBAL OPTIONS CONFIGURATION
 -- Some prefer space as the map leader, but why
-conf.mapleader = ","
-conf.maplocalleader = ","
+conf.mapleader = " "
+conf.maplocalleader = " "
 -- Toggle global status line
 conf.global_statusline = true
 -- set numbered lines
 conf.number = true
 -- enable mouse see :h mouse
-conf.mouse = "nv"
+conf.mouse = "a"
 -- set relative numbered lines
 conf.relative_number = true
 -- always show tabs; 0 never, 1 only if at least two tab pages, 2 always
@@ -43,9 +43,10 @@ conf.showtabline = 2
 conf.list = true
 -- which list chars to show
 conf.listchars = {
-  eol = "⤶",
-  tab = ">.",
-  trail = "~",
+  eol = "↲",
+  tab = "▸ ",
+  space = "·",
+  trail = "_",
   extends = "◀",
   precedes = "▶",
 }
@@ -58,18 +59,18 @@ conf.grepprg = "rg --hidden --vimgrep --smart-case --"
 -- Enable Github Copilot if you have an account, it is superior
 --
 -- Enable ChatGPT (set OPENAI_API_KEY environment variable)
-conf.enable_chatgpt = false
+conf.enable_chatgpt = true
 -- Enable Code Explain (requires 3.5GB model, uses GPT4ALL)
-conf.enable_codeexplain = false
+conf.enable_codeexplain = true
 -- Enable Codeium
-conf.enable_codeium = false
+conf.enable_codeium = true
 -- Enable Github Copilot
 conf.enable_copilot = false
 -- Enable Neoai, https://github.com/Bryley/neoai.nvim
 conf.enable_neoai = false
 --
 -- Enable display of ascii art
-conf.enable_asciiart = false
+conf.enable_asciiart = true
 -- Delete buffers and close files without closing your windows
 conf.enable_bbye = true
 -- Enable display of custom cheatsheets
@@ -77,18 +78,18 @@ conf.enable_cheatsheet = true
 -- Enable coding plugins for diagnostics, debugging, and language servers
 conf.enable_coding = true
 -- Enable compile plugin to compile and run current file
-conf.enable_compile = false
+conf.enable_compile = true
 -- Enable dressing plugin for improved default vim.ui interfaces
 conf.enable_dressing = true
 -- Enable easy motions, can be one of "hop", "leap", or "none"
-conf.enable_motion = "leap"
+conf.enable_motion = "none"
 -- Enable note making using Markdown preview and Obsidian plugins
-conf.enable_notes = false
+conf.enable_notes = true
 -- If notes enabled, markdown preview to use (preview, peek, none)
-conf.markdown_preview = "peek"
+conf.markdown_preview = "preview"
 -- If notes enabled, Neorg notes folders, multiple folders supported
 conf.neorg_notes = {
-  "~/Documents/Notes/Neorg", -- NEORG_NOTES
+  -- "~/Documents/Notes/Neorg", -- NEORG_NOTES
   -- "XXXXX", -- NEORG_NOTES
   -- "YYYYY", -- NEORG_NOTES
   -- "ZZZZZ", -- NEORG_NOTES
@@ -102,36 +103,32 @@ conf.enable_renamer = true
 -- Enable ranger in a floating window
 conf.enable_ranger_float = true
 -- Enable multiple cursors
-conf.enable_multi_cursor = false
+conf.enable_multi_cursor = true
 -- Highlight sections of code which might have security or quality issues
-conf.enable_securitree = false
+conf.enable_securitree = true
 -- neovim session manager to use: persistence, possession, or none
 conf.session_manager = "persistence"
 -- File explorer tree plugin: neo-tree, nvim-tree, or none
 conf.file_tree = "neo-tree"
 -- Replace the UI for messages, cmdline and the popupmenu
 conf.enable_noice = true
--- Enable ChatGPT (set OPENAI_API_KEY environment variable)
-conf.enable_chatgpt = true
--- Enable Code Explain (requires 3.5GB model, uses GPT4ALL)
-conf.enable_codeexplain = false
 -- Enable the newer rainbow treesitter delimiter highlighting
-conf.enable_rainbow2 = true
+conf.enable_rainbow2 = false
 -- Enable 'StartupTime' command
 conf.enable_startuptime = true
 -- Add/change/delete surrounding delimiter pairs with ease
 conf.enable_surround = true
 -- Enable the wilder plugin
-conf.enable_wilder = false
+conf.enable_wilder = true
 --
 -- Lualine, Tabline, and Winbar configuration
 --
 -- The Lualine style can be "free" or "onno"
 conf.lualine_style = "free"
 -- Separator for 'onno' style lualine components, can be "bubble" or "arrow"
-conf.lualine_separator = "bubble"
+conf.lualine_separator = "arrow"
 -- Enable fancy lualine components
-conf.enable_fancy = false
+conf.enable_fancy = true
 -- The statusline (lualine), tabline, and winbar can each be enabled or disabled
 -- Enable statusline (lualine)
 conf.enable_statusline = true
@@ -140,7 +137,7 @@ conf.enable_status_in_tab = false
 -- Enable winbar with navic location
 -- Can be one of "barbecue", "standard", or "none"
 -- Barbecue provides a clickable navic location, standard has more info
-conf.enable_winbar = "standard"
+conf.enable_winbar = "barbecue"
 -- Enable LSP progress in winbar
 conf.enable_lualine_lsp_progress = true
 -- Enable rebelot/terminal.nvim
@@ -149,24 +146,24 @@ conf.enable_terminal = true
 -- Enable toggleterm plugin
 conf.enable_toggleterm = true
 -- Enable playing games inside Neovim!
-conf.enable_games = true
+conf.enable_games = false
 -- Enable the WakaTime metrics dashboard (requires API key)
-conf.enable_wakatime = false
+conf.enable_wakatime = true
 -- Enable zen mode distraction-free coding
 conf.enable_zenmode = true
 -- if zenmode enabled then enable terminal support as well
 conf.enable_kitty = false
 conf.enable_alacritty = false
-conf.enable_wezterm = false
+conf.enable_wezterm = true
 -- Enable a dashboard, can be one of "alpha", "dash", "mini", or "none"
-conf.dashboard = "alpha"
+conf.dashboard = "dash"
 -- Number of recent files, dashboard header and quick links settings
 -- only apply to the Alpha dashboard
 -- Number of recent files shown in dashboard
 -- 0 disables showing recent files
-conf.dashboard_recent_files = 3
+conf.dashboard_recent_files = 5
 -- Enable the header of the dashboard
-conf.enable_dashboard_header = false
+conf.enable_dashboard_header = true
 -- Enable quick links of the dashboard
 conf.enable_dashboard_quick_links = true
 -- Enable either the Drop screensaver or the Zone screensaver
@@ -178,9 +175,9 @@ conf.enable_screensaver = "none"
 -- Screensaver timeout in minutes
 conf.screensaver_timeout = 15
 -- Enable the Neovim bookmarks plugin (https://github.com/ldelossa/nvim-ide)
-conf.enable_bookmarks = false
+conf.enable_bookmarks = true
 -- Enable the Neovim IDE plugin (https://github.com/ldelossa/nvim-ide)
-conf.enable_ide = false
+conf.enable_ide = true
 -- Enable Navigator
 conf.enable_navigator = true
 -- Enable Project manager
@@ -188,40 +185,89 @@ conf.enable_project = true
 -- Enable window picker
 conf.enable_picker = true
 -- Enable smooth scrolling with neoscroll plugin
-conf.enable_smooth_scrolling = true
+conf.enable_smooth_scrolling = false
 -- Enable the Neotest plugin
-conf.enable_neotest = false
+conf.enable_neotest = true
 
 -- PLUGINS CONFIGURATION
 -- media backend, one of "ueberzug"|"viu"|"chafa"|"jp2a"|"catimg"|"none"
 conf.media_backend = "jp2a"
 -- Style of indentation, can be one of:
 -- 'background' colored' 'context' 'listchars' 'mini' 'simple' 'none'
-conf.indentline_style = "mini"
+conf.indentline_style = "context"
 -- treesitter parsers to be installed
 conf.treesitter_ensure_installed = {
-  "bash",
-  "cpp",
-  "go",
-  "graphql",
-  "html",
-  "java",
-  "javascript",
-  "json",
+  "c",
   "lua",
-  "markdown",
-  "markdown_inline",
-  "query",
-  "php",
-  "python",
-  "regex",
-  "rust",
-  "scss",
-  "toml",
-  "tsx",
-  "typescript",
   "vim",
   "vimdoc",
+  "query",
+  "cpp",
+  "awk",
+  "bash",
+  "comment",
+  "commonlisp",
+  "css",
+  "csv",
+  "diff",
+  "dockerfile",
+  "dot",
+  "fennel",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "go",
+  "gomod",
+  "gosum",
+  "gowork",
+  "gpg",
+  "graphql",
+  "hcl",
+  "html",
+  "htmldjango",
+  "http",
+  "hurl",
+  "ini",
+  "java",
+  "javascript",
+  "jq",
+  "jsdoc",
+  "json",
+  "jsonc",
+  "julia",
+  "latex",
+  "luadoc",
+  "luap",
+  "make",
+  "markdown",
+  "markdown_inline",
+  "mermaid",
+  "ninja",
+  "nix",
+  "passwd",
+  "pem",
+  "perl",
+  "php",
+  "puppet",
+  "python",
+  "regex",
+  "requirements",
+  "robot",
+  "rst",
+  "ruby",
+  "rust",
+  "scss",
+  "smithy",
+  "sql",
+  "terraform",
+  "todotxt",
+  "toml",
+  "tsv",
+  "tsx",
+  "typescript",
+  "xml",
   "yaml",
 }
 -- Enable clangd or ccls for C/C++ diagnostics
@@ -231,52 +277,63 @@ conf.enable_clangd = false
 -- LSPs that should be installed by Mason-lspconfig
 -- Leave the 'LSP_SERVERS' trailing comment, it is used by lazyman
 conf.lsp_servers = {
-  "bashls", -- LSP_SERVERS
+  "awk_ls",        -- LSP_SERVERS
+  "bashls",        -- LSP_SERVERS
   "cssmodules_ls", -- LSP_SERVERS
-  "denols", -- LSP_SERVERS
-  "dockerls", -- LSP_SERVERS
+  "denols",        -- LSP_SERVERS
+  "dockerls",      -- LSP_SERVERS
   -- "eslint",     -- LSP_SERVERS
-  "gopls", -- LSP_SERVERS
-  "graphql", -- LSP_SERVERS
-  "html", -- LSP_SERVERS
-  "jdtls", -- LSP_SERVERS
-  "jsonls", -- LSP_SERVERS
-  "julials", -- LSP_SERVERS
-  "ltex", -- LSP_SERVERS
-  "lua_ls", -- LSP_SERVERS
-  "marksman", -- LSP_SERVERS
-  "pylsp", -- LSP_SERVERS
-  "pyright", -- LSP_SERVERS
-  "sqlls", -- LSP_SERVERS
-  "tailwindcss", -- LSP_SERVERS
-  "texlab", -- LSP_SERVERS
-  "tsserver", -- LSP_SERVERS
-  "vimls", -- LSP_SERVERS
-  "yamlls", -- LSP_SERVERS
+  "gopls",         -- LSP_SERVERS
+  "graphql",       -- LSP_SERVERS
+  "helm_ls",       -- LSP_SERVERS
+  "html",          -- LSP_SERVERS
+  "jdtls",         -- LSP_SERVERS
+  "jsonls",        -- LSP_SERVERS
+  "jqls",          -- LSP_SERVERS
+  "julials",       -- LSP_SERVERS
+  "ltex",          -- LSP_SERVERS
+  "lua_ls",        -- LSP_SERVERS
+  "marksman",      -- LSP_SERVERS
+  "pylsp",         -- LSP_SERVERS
+  "pyright",       -- LSP_SERVERS
+  "sqlls",         -- LSP_SERVERS
+  "tailwindcss",   -- LSP_SERVERS
+  "taplo",         -- LSP_SERVERS
+  "texlab",        -- LSP_SERVERS
+  "terraformls",   -- LSP_SERVERS
+  "tsserver",      -- LSP_SERVERS
+  "vimls",         -- LSP_SERVERS
+  "yamlls",        -- LSP_SERVERS
 }
 -- Formatters and linters installed by Mason
 conf.formatters_linters = {
-  "actionlint", -- FORMATTERS_LINTERS
-  "gofumpt", -- FORMATTERS_LINTERS
-  "goimports", -- FORMATTERS_LINTERS
-  "golines", -- FORMATTERS_LINTERS
-  "golangci-lint", -- FORMATTERS_LINTERS
+  "actionlint",         -- FORMATTERS_LINTERS
+  "djlint",             -- FORMATTERS_LINTERS
+  "gitlint",            -- FORMATTERS_LINTERS
+  "gofumpt",            -- FORMATTERS_LINTERS
+  "goimports",          -- FORMATTERS_LINTERS
+  "golines",            -- FORMATTERS_LINTERS
+  "golangci-lint",      -- FORMATTERS_LINTERS
+  "hadolint",           -- FORMATTERS_LINTERS
   "google-java-format", -- FORMATTERS_LINTERS
-  "latexindent", -- FORMATTERS_LINTERS
-  "markdownlint", -- FORMATTERS_LINTERS
-  "prettier", -- FORMATTERS_LINTERS
-  "sql-formatter", -- FORMATTERS_LINTERS
-  -- "shellcheck", -- FORMATTERS_LINTERS
+  "latexindent",        -- FORMATTERS_LINTERS
+  -- "markdownlint",       -- FORMATTERS_LINTERS
+  "prettier",           -- FORMATTERS_LINTERS
+  "sql-formatter",      -- FORMATTERS_LINTERS
+  "shellcheck",      -- FORMATTERS_LINTERS
+  "shellharden",     -- FORMATTERS_LINTERS
   -- "shfmt",           -- FORMATTERS_LINTERS
-  "stylua", -- FORMATTERS_LINTERS
-  "tflint", -- FORMATTERS_LINTERS
+  "stylua",          -- FORMATTERS_LINTERS
+  "tflint",   -- FORMATTERS_LINTERS
+  "tfsec",    -- FORMATTERS_LINTERS
+  "vint",     -- FORMATTERS_LINTERS
   "yamllint", -- FORMATTERS_LINTERS
 }
 -- Formatters and linters installed externally
 conf.external_formatters = {
-  -- "beautysh",        -- FORMATTERS_LINTERS
+  "beautysh",        -- FORMATTERS_LINTERS
   "black", -- FORMATTERS_LINTERS
-  "ruff", -- FORMATTERS_LINTERS
+  "ruff",  -- FORMATTERS_LINTERS
 }
 -- enable greping in hidden files
 conf.telescope_grep_hidden = true
@@ -284,10 +341,11 @@ conf.telescope_grep_hidden = true
 --   "none":  diagnostics are disabled but still underlined
 --   "icons": only an icon will show, use ',de' to see the diagnostic
 --   "popup": an icon will show and a popup with the diagnostic will appear
-conf.show_diagnostics = "icons"
+conf.show_diagnostics = "popup"
 -- Enable semantic highlighting
 conf.enable_semantic_highlighting = true
 -- Convert semantic highlights to treesitter highlights
 conf.convert_semantic_highlighting = true
 
 return conf
+
